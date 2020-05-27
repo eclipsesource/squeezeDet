d30018569b6f4559cf179ea63a259bdeb37618b2
XintianShi <xintianshi@hotmail.com>
2020-05-27 15:28:46 +0200

    Enable freezing graph with gpu
    Enable freezing graph with gpu, since the model was trained on gpu.

--- src/eval_freeze.py
+++ src/eval_freeze.py
@@ -1,95 +1,88 @@
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
+import cv2
+from datetime import datetime
 import os.path
 import sys
 import time
-from datetime import datetime
-from pathlib import Path
+from utils.util import *
 from typing import List
-from dataset import pascal_voc, kitti
-
-import cv2
-import numpy as np
-import tensorflow as tf
-from six.moves import xrange
 from tensorflow import lite
-from tensorflow.python.platform import gfile
+
+
+import numpy as np
+from six.moves import xrange
+import tensorflow as tf
 
 from config import *
+from dataset import pascal_voc, kitti
+from utils.util import bbox_transform, Timer
 from nets import *
-from utils.util import Timer, bbox_transform
+from tensorflow.python.platform import gfile
+from pathlib import Path
 
-tf.app.flags.DEFINE_string('checkpoint_path', 'tmp/logs/train/model.ckpt-20',
-                            """Path to the selected checkpoint.""")
-tf.app.flags.DEFINE_string('frozen_graph_name', 'freeze.pb',
-                            """Path to the frozen graph.""")
-tf.app.flags.DEFINE_string('tflite_name', 'mobile_model.tflite',
-                            """Path to the tflite model.""")
-tf.app.flags.DEFINE_string('data_path', 'data/KITTI', """Root directory of data""")
+
 
 FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('checkpoint_path', 'tmp/logs/train/model.ckpt-220', """Path to the selected checkpoint.""")
+tf.app.flags.DEFINE_string('frozen_graph_name', 'freeze.pb', """Path to the frozen graph.""")
+tf.app.flags.DEFINE_string('tflite_name', 'mobile_model.tflite', """Path to the tflite model.""")
+tf.app.flags.DEFINE_string('data_path', 'data/KITTI', """Root directory of data""")
+tf.app.flags.DEFINE_string('gpu', '0', """gpu id.""")
 
-def main(argv=None):  # pylint: disable=unused-argument
-    freezing_graph = tf.Graph() # initialize tf computation graph
-    with freezing_graph.as_default(): # set this graph as default graph
-        mc = kitti_squeezeDetPlus_config() # get model and config
-        mc.LOAD_PRETRAINED_MODEL = False
-        #mc.BATCH_SIZE=4
-        model = SqueezeDetPlus(mc, state=val)
-        #with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
-            # Restores from checkpoint
-            #print(sess.run([v for v in tf.trainable_variables() if v.name == "fire2/squeeze1x1/biases:0"][0]))
-            #print('\n\n')
-            #saver.restore(sess, FLAGS.checkpoint_path)  
+def eval_once(
+    saver, ckpt_path, imdb,
+    model, output_node_names):
+  gpu_config = tf.ConfigProto(allow_soft_placement=True)
+  gpu_config.gpu_options.allow_growth = True
 
-        #images = tf.placeholder(dtype=tf.float32, shape = tf.TensorShape([None, None, None, 1]), name = 'in_images')
-        #model.image_input = tf.identity(model.image_input, name='in_images')# images       
-        output_node_names = ["out_det_probs", "out_det_boxes", "out_det_class"]
-        det_probs = tf.identity(model.det_probs, name = output_node_names[0])
-        det_boxes = tf.identity(model.det_boxes, name = output_node_names[1])
-        det_class = tf.identity(model.det_class, name = output_node_names[2])
-        
-    _freeze_graph(freezing_graph, FLAGS.checkpoint_path, FLAGS.frozen_graph_name, output_node_names, mc, model)
-    _convert_pb_to_tflite(FLAGS.checkpoint_path, FLAGS.frozen_graph_name, FLAGS.tflite_name, output_node_names, model.image_input)
+  with tf.Session(config=gpu_config) as sess:
+    saver.restore(sess, ckpt_path)
+    images, scales = imdb.read_image_batch(shuffle=True)
+    det_boxes, det_probs, det_class = sess.run(
+        [model.det_boxes, model.det_probs, model.det_class],
+        feed_dict={model.ph_image_input:images})
+    output_graph_def = tf.graph_util.convert_variables_to_constants(
+            sess, sess.graph_def, output_node_names
+                )  # convert to constants?
+    frozen_graph_path = str(Path(ckpt_path).parents[2].joinpath(FLAGS.frozen_graph_name))
+    with gfile.GFile(frozen_graph_path, 'wb') as f:
+            f.write(output_graph_def.SerializeToString())
+    _convert_pb_to_tflite(frozen_graph_path=frozen_graph_path, input_shape= model.ph_image_input.shape)
 
-def _freeze_graph(graph, input_checkpoint, output_file_name: str, output_node_names: str, mc, model):
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
+def evaluate():
+  os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
+  with tf.Graph().as_default() as g:
+    mc = kitti_squeezeDetPlus_config()
+    mc.BATCH_SIZE = 1 # TODO(bichen): allow batch size > 1
+    mc.LOAD_PRETRAINED_MODEL = False
+    model = SqueezeDetPlus(mc, state='val')
+    output_node_names = ["out_det_probs", "out_det_boxes", "out_det_class"]
+    det_probs = tf.identity(model.det_probs, name=output_node_names[0])
+    det_boxes = tf.identity(model.det_boxes, name=output_node_names[1])
+    det_class = tf.identity(model.det_class, name=output_node_names[2])
     imdb = kitti('val', FLAGS.data_path, mc)
 
-    images, scales = imdb.read_image_batch(shuffle=False)
-    image = images[0][np.newaxis, ::] # numpy array
-    image = np.tile(image, [4, 1,1,1])
-    #image = tf.convert_to_tensor(image)
-    with tf.Session(graph=graph, config=config) as sess:
+    saver = tf.train.Saver(model.model_params)
+    eval_once(
+        saver, FLAGS.checkpoint_path, imdb, model, output_node_names)
 
-        sess.run(tf.global_variables_initializer())
-        restorer = tf.train.Saver(tf.global_variables())
-        restorer.restore(sess, input_checkpoint)
-        det_boxes, det_probs, det_class = sess.run(
-          [model.det_boxes, model.det_probs, model.det_class],
-          feed_dict={model.ph_image_input:image})
-        print(det_probs)
-
-        # tensor_name_list = [tensor.name for tensor in tf.get_default_graph().as_graph_def().node]
-        # for (i, tensor_name) in enumerate(tensor_name_list):
-        #     if i < 50:
-        #         print(tensor_name, '\n')
-
-        #print(tf.global_variables())
-        output_graph_def = tf.graph_util.convert_variables_to_constants(sess, sess.graph_def, output_node_names)
-        with gfile.GFile(str(Path(input_checkpoint).parents[2].joinpath(output_file_name)), 'wb') as f:
-            f.write(output_graph_def.SerializeToString())
-
-def _convert_pb_to_tflite(input_checkpoint:str, frozen_graph_name: str, tflite_file_name: str, output_node_names: List[str], image):
-    converter = lite.TFLiteConverter.from_frozen_graph(str(Path(input_checkpoint).parents[2].joinpath(frozen_graph_name)),
-                                                       ['image_input'],
-                                                       output_node_names,
-                                                       input_shapes={'image_input':image.shape})
+def _convert_pb_to_tflite(
+    frozen_graph_path: str,
+    input_shape: List[int],
+    input_node_names: List[str] = ['image_input'],
+    output_node_names: List[str] = ["out_det_probs", "out_det_boxes", "out_det_class"]
+        ):
+    converter = lite.TFLiteConverter.from_frozen_graph(
+        frozen_graph_path, input_node_names, output_node_names, input_shapes={'image_input':input_shape}
+            )
     tflite_model = converter.convert()
-    open(str(Path(input_checkpoint).parents[2].joinpath(tflite_file_name)), "wb").write(tflite_model)
+    open(str(Path(frozen_graph_path).parent.joinpath('tflite_model.tflite')), "wb").write(tflite_model)
 
-
+def main(argv=None):  # pylint: disable=unused-argument
+  evaluate()
 
 
 if __name__ == '__main__':
